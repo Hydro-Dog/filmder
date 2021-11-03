c62935aa22bc337bdad5603a98276da2bd134988
Vlad <vladbabaev11@gmail.com>
2021-11-03 22:32:30 +0300

    android buid preparation

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.filmder.ionic',
+  appName: 'filmder',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
